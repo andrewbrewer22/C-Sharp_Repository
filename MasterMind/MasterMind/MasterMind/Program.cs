b80ca0e05783fb034b47cf0c691e0e83c978e1cc
Andrew <andrewwilsonbrewer22@gmail.com>
2021-02-05 19:54:52 -0800

    20200205 MasterMine 1.0 INCOMPLETE

--- MasterMind/MasterMind/MasterMind/Program.cs
+++ MasterMind/MasterMind/MasterMind/Program.cs
@@ -14,8 +14,7 @@ namespace MasterMind
             int positionOne = Convert.ToInt32(Randomizer());
             int positionTwo = Convert.ToInt32(Randomizer());
 
-            Console.WriteLine(positionOne);
-            Console.WriteLine(positionTwo);
+            Console.WriteLine("PosOne: " + positionOne + " PosTwo: " + positionTwo);
 
             Console.WriteLine("Welcome to Master Mind");
             Console.WriteLine("Possible Colors: Red, Green, Blue");
@@ -37,7 +36,8 @@ namespace MasterMind
                     Console.WriteLine("Invalid Input");
                 }
 
-                CheckAnswer(positionOne, positionTwo, guess1, guess2);
+                Console.WriteLine(CheckAnswer(positionOne, positionTwo, guess1, guess2));
+                Console.WriteLine("________________________");
             }
             while (playing);
         }
@@ -78,7 +78,7 @@ namespace MasterMind
         static string Randomizer()
         {
             Random random = new Random();
-            int finalValue = random.Next(1, 4);
+            int finalValue = random.Next(0, 3);
 
             return finalValue.ToString();
         }
@@ -90,7 +90,54 @@ namespace MasterMind
             "0-1" if they guessed one of the colors correctly at the correct position
             "2-0" if they guessed both colors correctly but at the wrong positions*/
 
-            return "";
+            if (positionOne == positionTwo)
+            {
+                if (guess1 == guess2)
+                {
+                    playing = false;
+                    return "Correct!";
+                }
+                else
+                {
+                    return "2-0: guessed both colors correctly but at the wrong positions";
+                }
+            }
+
+            if (guess1 == guess2)
+            {
+                if (positionOne == guess2 && positionTwo == guess1)
+                {
+                    return "2-0: guessed both colors correctly but at the wrong positions";
+                }
+
+                else if (positionOne == guess1 || positionTwo == guess1)
+                {
+                    return "0-1: guessed one of the colors correctly at the correct position";
+                }
+
+                else
+                {
+                    //playing = false;
+                    return "0-0";
+                }
+            }
+
+            if (positionOne == guess1)
+            {
+                if (positionTwo == guess2)
+                {
+                    playing = false;
+                    return "Correct!";
+                }
+                return "0-1: guessed one of the colors correctly at the correct position";
+            }
+
+            if (positionOne == guess2 || positionTwo == guess1)
+            {
+                return "1-0: guessed one of the colors correctly but not at the correct position";
+            }
+
+            return "0-0: User did not guess the correct colors at all";
         }
     }
 }
