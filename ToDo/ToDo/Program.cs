62e02bbbf0f5c537c123ea07090dd57a44184fa1
Andrew <andrewwilsonbrewer22@gmail.com>
2021-02-10 19:58:55 -0800

    20210210 ToDoGenerator 1.0

---
+++ ToDo/ToDo/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+/*1. Ask the user if they want to add an item, or if they are done.
+2. If the user wants to add an item,ask for description, due date, and priority.
+create a ToDoItem representing the user's input, add the new instance to a list of items
+3. Repeat until the user is done entering items.
+4. Loop through the items and print the details of each item to the console.*/
+
+
+namespace ToDo
+{
+    class Program
+    {
+        public static bool gatherData = true;
+        static void Main()
+        {
+            Console.WriteLine("Known bugs: ");
+            Console.WriteLine("Typing Done will not end the current iteration," +
+                " Must go through all iterations before ending");
+           // Console.WriteLine("");
+            Console.WriteLine("Prints done and whatever else was typed when trying" +
+                " to exit the toDo creater");
+            Console.WriteLine("");
+            Console.WriteLine("The ToDo generator \"Works\" but needs some improvments" );
+            Console.WriteLine("");
+
+            ToDoItem item = new ToDoItem();
+
+            do
+            {
+                Console.WriteLine("Type \"Done\" when finished inputing data");
+                Console.WriteLine("Enter a Description");
+                item.Description = Console.ReadLine();
+                //gatherData = item.CreateToDoItem(item.Description, item.DueDate, item.Priority);
+
+                Console.WriteLine("Enter a priority: High, Med, Low");
+                item.Priority = Console.ReadLine();
+                //gatherData = item.CreateToDoItem(item.Description, item.DueDate, item.Priority);
+
+                Console.WriteLine("Enter a due date");
+                item.DueDate = Console.ReadLine();
+
+                //item.CreateToDoItem(item.Description, item.DueDate, item.Priority);
+                gatherData = item.CreateToDoItem(item.Description, item.DueDate, item.Priority);
+            }
+            while (gatherData);
+
+            
+        }
+    }
+
+    class ToDoItem
+    {
+        public string Description { get; set; }
+        public string DueDate { get; set; }
+        public string Priority { get; set; }
+
+        List<string> resultList = new List<string>();
+        public bool CreateToDoItem(string description, string dueDate, string priority)
+        {
+            //set our values
+            Description = description;
+            DueDate = dueDate;
+            Priority = priority;
+
+            bool keepGathering = true;
+            if(description == "Done" || dueDate == "Done" || priority == "Done")
+            {
+                keepGathering = false;
+            }
+
+            //Our List that we will use at the end to be printed
+            
+            resultList.Add(description + " " + dueDate + " " + priority);
+            ConstructList(keepGathering, resultList);
+            /*
+            for (int i = 0; i <= resultList.Count - 1; i++)
+            {
+                Console.WriteLine(resultList[i]);
+            }
+            */
+
+            return keepGathering;
+        }
+
+        public void ConstructList(bool gatherData, List<string> ourList)
+        {
+
+            if (gatherData == false)
+            {
+                //Loop through our entire list and print every index
+                for (int i = 0; i <= ourList.Count -1; i++)
+                {
+                    Console.WriteLine(ourList[i]);
+                }
+            }
+        }
+    }
+}
--- TowersOfHanoi/TowersOfHanoi/Program.cs
+++ TowersOfHanoi/TowersOfHanoi/Program.cs
@@ -7,6 +7,11 @@ Your program should update the board
 Your program should repeat steps 1,2 and 3 until the game is won.
 Your program should not allow a user to make an illegal move.*/
 
+
+//Things to improve
+//1. Create a user input method that regulates and tests all user input validity
+//2.
+
 namespace TowersOfHanoi
 {
     class Program
@@ -15,6 +20,8 @@ namespace TowersOfHanoi
         static public int[] stickA = new int[] { 4, 3, 2, 1 };
         static public int[] stickB = new int[] { 0, 0, 0, 0 };
         static public int[] stickC = new int[] { 0, 0, 0, 0 };
+
+        static public int[] defaultStick = new int[] { 0, 0, 0, 0 };
 
         //Dictionary used to pair our sticks with int values for easier access
         static Dictionary<int, int[]> stickDictionary = new Dictionary<int, int[]>();
@@ -28,6 +35,7 @@ namespace TowersOfHanoi
             stickDictionary.Add(0, stickA);
             stickDictionary.Add(1, stickB);
             stickDictionary.Add(2, stickC);
+            stickDictionary.Add(10, defaultStick);
 
             Console.WriteLine("Welcome to Towers of Hanoi");
             Console.WriteLine("Rules in ReadMe file");
@@ -38,17 +46,28 @@ namespace TowersOfHanoi
                 PrintBoard(stickA, stickB, stickC);
                 Console.WriteLine("Choose a stick: (1) (2) (3)");
                 userInputPosition = Console.ReadLine();
-                userPosition = CheckPosition(userInputPosition, stickA, stickB, stickC);
-
-                //If our stick we chose is empty, keep choosing a stick until a valid choice is picked
-                do
+                userPosition = CheckPosition(userInputPosition, stickA, stickB, stickC).Item1;
+                //While stick invalid, allow user to re-input until valid
+                while (userPosition == -1)
                 {
                     userInputPosition = Console.ReadLine();
-                    userPosition = CheckPosition(userInputPosition, stickA, stickB, stickC);
+                    userPosition = CheckPosition(userInputPosition, stickA, stickB, stickC).Item1;
                 }
-                while (userPosition == -1);
 
+                //Checks for desired user stick to move value
+                Console.WriteLine("Ok, choose the destination of your choice");
+                Console.WriteLine("_________________________________________");
+                int movingStick = 0;
+                string inputMovingStick = "";
+                inputMovingStick = Console.ReadLine();
+                movingStick = MovePosition(inputMovingStick, userPosition);
 
+                while (movingStick == -1)
+                {
+                    Console.WriteLine("Choose valid destination");
+                    inputMovingStick = Console.ReadLine();
+                    movingStick = MovePosition(inputMovingStick, userPosition);
+                }
 
             }
             while (playing);
@@ -63,37 +82,46 @@ namespace TowersOfHanoi
         }
         #endregion
 
-        #region Postion Validation
-        static int CheckPosition(string userInput, int[] stickA, int[] stickB, int[] stickC)
+        #region Postion Affairs
+        static (int, int) CheckPosition(string userInput, int[] stickA, int[] stickB, int[] stickC)
         {
-            int userChance = 0;
+            int stickTopNumber = 0;
+            int stick = 0;
             //If assigning userInput from string to int is feasible
             try
             {
-                userChance = Convert.ToInt32(userInput);
+                //if the following line of code can execute than no problem
+                stick = Convert.ToInt32(userInput);
+                //This is if the user inputs nothing and presses enter
+                if (userInput == "")
+                {
+                    Console.WriteLine("Invalid Input");
+                    return (-1, 0);
+                    stick = 0;
+                }
             }
             catch
             {
                 Console.WriteLine("Invalid Input");
-                userChance = 0;
+                stick = 0;
             }
             //If user int is a possible choice reassign to index value
-            switch (userChance)
+            switch (stick)
             {
                 case 1:
-                    userChance = 0;
+                    stick = 0;
                     break;
 
                 case 2:
-                    userChance = 1;
+                    stick = 1;
                     break;
 
                 case 3:
-                    userChance = 2;
+                    stick = 2;
                     break;
 
                 default:
-                    userChance = 10;
+                    stick = 10;
                     Console.WriteLine("Invalid Input");
                     break;
             }
@@ -101,25 +129,82 @@ namespace TowersOfHanoi
             //Check if our number is equal to our sticks
 
             //Check if stick is empty
-            if(stickDictionary[userChance][0] == 0)
+            if (stickDictionary[stick][0] == 0)
             {
                 Console.WriteLine("Empty Stick");
                 Console.WriteLine("Choose valid Stick");
                 Console.WriteLine("___________________");
-                return -1;
+                return (-1, 0);
             }
+
+            //Loops through stick(s) and determines if it is empty
+            //if the stick is not empty grab the top valued index
             else
             {
-                for(int i = 0; i < stickDictionary[userChance].Length -1; i++)
+                //***Loop through the selected stick and sto the top value
+                for (int i = 3; i >= 0; i--)
                 {
-                    if(stickDictionary[userChance][i] == 0)
+                    //Loops through current stick arr identifying it has something
+                    if (stickDictionary[stick][i] != 0)
                     {
-
+                        //Loop through again to get the top of the stick
+                        for (int j = 0; j <= 3; j++)
+                        {
+                            //loops until local var finds an empty and take the prior value
+                            if (stickDictionary[stick][j] == 0)
+                            {
+                                stickTopNumber = stickDictionary[stick][i];
+                                break;
+                            }
+                            //else just take the last index of the stick
+                            else
+                            {
+                                stickTopNumber = stickDictionary[stick][3];
+                            }
+                        }
                     }
                 }
             }
 
-            return 124;
+            return (stickTopNumber, stick);
+        }
+
+        /*MovePosition() is in charge of checking the users
+         desired stick change, in which it is [EMPTY, FULL, SAME STICK]
+         We will than assign our picked stick index from the  prior method
+        and assign accordingly*/
+        static int MovePosition(string userInput, int userPosition)
+        {
+            //Test our user input and return -1 if the input is invalid
+            int checkInput = 0;
+            //If we can get throught the try catch, that means the input is valid
+            try
+            {
+                //if the following line of code can execute than no problem
+                checkInput = Convert.ToInt32(userInput);
+                //This is if the user inputs nothing and presses enter
+                if (userInput == "")
+                {
+                    Console.WriteLine("Invalid Input");
+                    checkInput = 0;
+                    return -1;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Input");
+                checkInput = 0;
+                return -1;
+            }
+
+            //Take our prior destination index out and replace with 0
+            //Put our index in our new destination if the destination is clear
+
+            //Check if the destination is clear by 
+            //checking if the top/ top most index is 0
+            for(int i = 3; i )
+            
+            return 21;
         }
         #endregion
     }
