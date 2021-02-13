8017e6e5869e2b01df35655924493db0c66e804a
Andrew <andrewwilsonbrewer22@gmail.com>
2021-02-13 02:01:54 -0800

    20210213 TowersOfHanoi 1.0

--- TowersOfHanoi/TowersOfHanoi/Program.cs
+++ TowersOfHanoi/TowersOfHanoi/Program.cs
@@ -16,6 +16,8 @@ namespace TowersOfHanoi
 {
     class Program
     {
+        static int numberOfMoves = 0;
+
         static public bool playing = true;
         static public int[] stickA = new int[] { 4, 3, 2, 1 };
         static public int[] stickB = new int[] { 0, 0, 0, 0 };
@@ -46,11 +48,15 @@ namespace TowersOfHanoi
                 PrintBoard(stickA, stickB, stickC);
                 Console.WriteLine("Choose a stick: (1) (2) (3)");
                 userInputPosition = Console.ReadLine();
+                //This is the number we grabbed from the top of the stick
                 userPosition = CheckPosition(userInputPosition, stickA, stickB, stickC).Item1;
                 //While stick invalid, allow user to re-input until valid
                 while (userPosition == -1)
                 {
+                    PrintBoard(stickA, stickB, stickC);
+
                     userInputPosition = Console.ReadLine();
+                    //This is the number we grabbed from the top of the stick
                     userPosition = CheckPosition(userInputPosition, stickA, stickB, stickC).Item1;
                 }
 
@@ -64,13 +70,24 @@ namespace TowersOfHanoi
 
                 while (movingStick == -1)
                 {
+                    PrintBoard(stickA, stickB, stickC);
                     Console.WriteLine("Choose valid destination");
                     inputMovingStick = Console.ReadLine();
                     movingStick = MovePosition(inputMovingStick, userPosition);
                 }
-
+                if(stickDictionary[2][0] == 4 && stickDictionary[2][1] == 3 &&
+                    stickDictionary[2][2] == 2 && stickDictionary[2][3] == 1)
+                {
+                    playing = false;
+                }
+                Console.WriteLine("Number of Moves: " + numberOfMoves);
             }
             while (playing);
+
+            PrintBoard(stickA, stickB, stickC);
+            Console.WriteLine("Number of Moves: " + numberOfMoves);
+            Console.WriteLine("You Win!");
+            
         }
 
         #region Print Board
@@ -150,22 +167,30 @@ namespace TowersOfHanoi
                         //Loop through again to get the top of the stick
                         for (int j = 0; j <= 3; j++)
                         {
+                            //Console.WriteLine("DEBUG 1: " + "Top Number " + stickTopNumber);
                             //loops until local var finds an empty and take the prior value
                             if (stickDictionary[stick][j] == 0)
                             {
                                 stickTopNumber = stickDictionary[stick][i];
+                                stickDictionary[stick][i] = 0;
+                                //Console.WriteLine("DEBUG 2: " + "Top Number " + stickTopNumber);
                                 break;
                             }
                             //else just take the last index of the stick
                             else
                             {
-                                stickTopNumber = stickDictionary[stick][3];
+                                stickTopNumber = stickDictionary[stick][i];
+                               // Console.WriteLine("DEBUG 3: " + "Top Number " + stickTopNumber);
+                                stickDictionary[stick][i] = 0;
+                                break;
                             }
                         }
+                        break;
                     }
                 }
             }
 
+            //Console.WriteLine("DEBUG 4: " + "Top Number " + stickTopNumber);
             return (stickTopNumber, stick);
         }
 
@@ -173,28 +198,61 @@ namespace TowersOfHanoi
          desired stick change, in which it is [EMPTY, FULL, SAME STICK]
          We will than assign our picked stick index from the  prior method
         and assign accordingly*/
+
+        //userInput is the desired location for the userPosition. The userPosition is the top stick number we just grabbed
         static int MovePosition(string userInput, int userPosition)
         {
             //Test our user input and return -1 if the input is invalid
-            int checkInput = 0;
+            int currentRow = 0;
             //If we can get throught the try catch, that means the input is valid
             try
             {
                 //if the following line of code can execute than no problem
-                checkInput = Convert.ToInt32(userInput);
+                currentRow = Convert.ToInt32(userInput);
                 //This is if the user inputs nothing and presses enter
                 if (userInput == "")
                 {
                     Console.WriteLine("Invalid Input");
-                    checkInput = 0;
+                    currentRow = 0;
+                    return -1;
+                }
+                else if (currentRow == 1 || currentRow == 2 || currentRow == 3)
+                {
+                    //Console.WriteLine("Valid Input");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                    currentRow = 0;
                     return -1;
                 }
             }
             catch
             {
                 Console.WriteLine("Invalid Input");
-                checkInput = 0;
+                currentRow = 0;
                 return -1;
+            }
+
+            //If user int is a possible choice reassign to index value
+            switch (currentRow)
+            {
+                case 1:
+                    currentRow = 0;
+                    break;
+
+                case 2:
+                    currentRow = 1;
+                    break;
+
+                case 3:
+                    currentRow = 2;
+                    break;
+
+                default:
+                    currentRow = 10;
+                    Console.WriteLine("Invalid Input");
+                    break;
             }
 
             //Take our prior destination index out and replace with 0
@@ -202,9 +260,40 @@ namespace TowersOfHanoi
 
             //Check if the destination is clear by 
             //checking if the top/ top most index is 0
-            for(int i = 3; i )
-            
-            return 21;
+            for (int i = 0; i <= 3; i++)
+            {
+                //if our currently selected row has room, check if prior number is
+                //bigger to allow to be stacked on, if all row is 0 than add to 0 index
+                //stick empty
+                if (stickDictionary[currentRow][0] == 0)
+                {
+                    stickDictionary[currentRow][0] = userPosition;
+                    numberOfMoves += 1;
+                    return 1;
+                }
+                //stick full
+                else if(stickDictionary[currentRow][3] != 0)
+                {
+                    Console.WriteLine("Stick Full");
+                    return -1;
+                }
+
+                else if(stickDictionary[currentRow][i] == 0)
+                {
+                    if(userPosition <= stickDictionary[currentRow][i -1])
+                    {
+                        stickDictionary[currentRow][i] = userPosition;
+                        numberOfMoves += 1;
+                        return 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Prior number in destination is smaller than current selection");
+                        return -1;
+                    }   
+                }
+            }
+            return -1;
         }
         #endregion
     }
